c1a4a0a13e51d4cd6f48f3dc3bb7612b32cccc4b
Sviat98 <bashkevich.98@gmail.com>
2025-11-19 14:37:14 +0300

    Added PRIMARY KEY player_id to player_auth table

--- src/main/kotlin/com/bashkevich/tennisscorekeeperbackend/model/auth/PlayerAuthTable.kt
+++ src/main/kotlin/com/bashkevich/tennisscorekeeperbackend/model/auth/PlayerAuthTable.kt
@@ -4,8 +4,10 @@ import com.bashkevich.tennisscorekeeperbackend.model.player.PlayerTable
 import org.jetbrains.exposed.v1.core.Table
 
 object PlayerAuthTable: Table("player_auth"){
-    val playerId = reference("player_id", PlayerTable).uniqueIndex()
+    val playerId = reference("player_id", PlayerTable)
     val login = varchar("login", 50)
     val password = binary("password")
     val isAdmin = bool("is_admin")
+
+    override val primaryKey = PrimaryKey(playerId)
 }
