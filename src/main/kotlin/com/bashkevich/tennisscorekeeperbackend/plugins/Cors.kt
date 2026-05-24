ab6e1904cc1a46e5c96c7896677ecb874b57cd1e
Sviat98 <bashkevich.98@gmail.com>
2026-05-24 16:39:32 +0300

    Исправлены настройки CORS (0.0.0.0-> 127.0.0.1)

--- src/main/kotlin/com/bashkevich/tennisscorekeeperbackend/plugins/Cors.kt
+++ src/main/kotlin/com/bashkevich/tennisscorekeeperbackend/plugins/Cors.kt
@@ -10,8 +10,8 @@ fun Application.configureCors(){
     install(CORS){
         allowHost("tennisscorekeeper.onrender.com")
         allowHost("tennisscorekeeper.tech")
-        allowHost("0.0.0.0:8080")
-        allowHost("0.0.0.0:8081")
+        allowHost("127.0.0.1:8080")
+        allowHost("127.0.0.1:8081")
         allowHeader(HttpHeaders.Authorization)
         allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.AccessControlAllowOrigin)
